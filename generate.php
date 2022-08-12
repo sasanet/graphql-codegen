9689be62fbdf1444f14778759e79ebc71749fba4
Marco Santantonio <marco.santantonio@innovaomnia.it>
2022-08-12 22:00:57 +0200

    defautl typeMapping.

--- generate.php
+++ generate.php
@@ -20,22 +20,18 @@ $modules = [
         namespace: 'Arxy\GraphQLCodegen\Tests\Module1\Expected',
         typeMapping: [
             'DateTime' => DateTimeInterface::class,
+            'LocalDate' => DateTimeInterface::class,
+            'LocalDateTime' => DateTimeInterface::class,
+            'Long'=> "int"
         ],
         directory: __DIR__ . '/tests/Module1/Expected'
-    ),
-    new Module(
-        name: 'module2',
-        schema: __DIR__ . '/tests/Module2/schema.graphql',
-        namespace: 'Arxy\GraphQLCodegen\Tests\Module2\Expected',
-        typeMapping: [],
-        directory: __DIR__ . '/tests/Module2/Expected'
-    ),
+    )
 ];
 
 foreach ($modules as $module) {
     file_put_contents(
         dirname($module->getSchema()) . '/ast.php',
-        "<?php\n\ndeclare(strict_types=1);\n\nreturn " . var_export(
+        "<?php\n\nreturn " . var_export(
             AST::toArray(
                 Parser::parse(
                     file_get_contents(
